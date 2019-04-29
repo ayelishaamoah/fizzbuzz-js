53cdcaa2603a7c4feea6fc3d0e05cb4bd6c5cebd
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-29 17:23:31 +0100

    Added fizzbuzz test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,10 +2,14 @@
 
 describe("fizzBuzz", function() {
   it('prints fizz if 3 is entered', function() {
-    expect(fizzBuzz(3)).toEqual("Fizz")
+    expect(fizzBuzz(3)).toEqual("Fizz");
   });
 
   it('prints buzz if 5 is entered', function() {
-    expect(fizzBuzz(5)).toEqual("Buzz")
+    expect(fizzBuzz(5)).toEqual("Buzz");
+  });
+
+  it('prints fizzbuzz if  is entered', function() {
+    expect(fizzBuzz(15)).toEqual("FizzBuzz");
   });
 });
