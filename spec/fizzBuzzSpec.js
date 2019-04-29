6e6594d758349f5a9c607cd5215f275865221cc1
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-29 17:01:23 +0100

    Added test for Buzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -4,4 +4,8 @@ describe("fizzBuzz", function() {
   it('prints fizz if 3 is entered', function() {
     expect(fizzBuzz(3)).toEqual("Fizz")
   });
+
+  it('prints buzz if 5 is entered', function() {
+    expect(fizzBuzz(5)).toEqual("Buzz")
+  });
 });
