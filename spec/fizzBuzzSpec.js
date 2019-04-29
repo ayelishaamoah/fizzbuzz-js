01ef88ebec0a2045d9712c7d813db750a467dbc2
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-29 16:54:40 +0100

    Added first test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,1 +1,7 @@
+// expect it to print numbers from one to 100
 
+describe("fizzBuzz", function() {
+  it('prints fizz if 3 is entered', function() {
+    expect(fizzBuzz(3)).toEqual("Fizz")
+  });
+});
