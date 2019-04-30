48f8cb20124b4c0ee0088417101563646e7696e6
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-30 13:00:39 +0100

    Added printing non FizzBuzz numbers

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -9,11 +9,15 @@ describe("fizzBuzz", function() {
     expect(fizzBuzz(5)).toEqual("Buzz");
   });
 
-  it('prints fizzbuzz if  is entered', function() {
+  it('prints fizzbuzz if 15 is entered', function() {
     expect(fizzBuzz(15)).toEqual("FizzBuzz");
   });
 
-  it('prints fizzbuzz if  is entered', function() {
+  it('prints fizzbuzz if 6 is entered', function() {
     expect(fizzBuzz(6)).toEqual("Fizz");
+  });
+
+  it('prints 1 if 1 is entered', function() {
+    expect(fizzBuzz(1)).toEqual(1);
   });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,8 +2,10 @@ function fizzBuzz(number) {
   if (number % 3 == 0 && number % 5 == 0) {
     return "FizzBuzz";
   } else if (number % 3 == 0) {
-    return "Fizz"
-  } else if (number == 5) {
-    return "Buzz"
+    return "Fizz";
+  } else if (number % 5 == 0) {
+    return "Buzz";
+  } else {
+    return number
   }
 }
